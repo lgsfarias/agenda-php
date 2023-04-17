41b2dbaefeda89074be58a02a641016b5dcdc762
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-04-16 21:41:15 -0300

    feat: selecting a contact by id

--- config/process.php
+++ config/process.php
@@ -3,7 +3,18 @@ session_start();
 include_once 'connection.php';
 include_once 'url.php';
 
-$contacts = [];
+$id;
+$contact = [];
+
+if (isset($_GET['id'])) {
+  $id = $_GET['id'];
+  $sql = "SELECT * FROM contacts WHERE id = :id";
+  $stmt = $conn->prepare($sql);
+  $stmt->bindParam(':id', $id);
+  $stmt->execute();
+  $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
 $sql = "SELECT * FROM contacts";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
--- index.php
+++ index.php
@@ -3,40 +3,40 @@ include_once 'templates/header.php';
 ?>
 <div class="container">
   <?php if (isset($sessionMsg) && $sessionMsg != '') : ?>
-    <p id="msg">
-      <?= $sessionMsg ?>
-    </p>
+  <p id="msg">
+    <?= $sessionMsg ?>
+  </p>
   <?php endif; ?>
   <h1 id="main-title">Minha Agenda</h1>
   <?php if (count($contacts) > 0) : ?>
-    <table class="table" id="contacts-table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Nome</th>
-          <th scope="col">Telefone</th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach ($contacts as $contact) : ?>
-          <tr>
-            <th scope="row"><?= $contact['id'] ?></th>
-            <td><?= $contact['name'] ?></td>
-            <td><?= $contact['phone'] ?></td>
-            <td class="actions">
-              <a href="#" class="btn btn-success">Visualizar</a>
-              <a href="#" class="btn btn-primary">Editar</a>
-              <a href="#" class="btn btn-danger">Excluir</a>
-            </td>
-          </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
+  <table class="table" id="contacts-table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Telefone</th>
+        <th scope="col"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($contacts as $contact) : ?>
+      <tr>
+        <th scope="row"><?= $contact['id'] ?></th>
+        <td><?= $contact['name'] ?></td>
+        <td><?= $contact['phone'] ?></td>
+        <td class="actions">
+          <a href="<?= $BASE_URL ?>show.php?id=<?= $contact['id'] ?>" class="btn btn-success">Visualizar</a>
+          <a href="#" class="btn btn-primary">Editar</a>
+          <a href="#" class="btn btn-danger">Excluir</a>
+        </td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
   <?php else : ?>
-    <p id="empty-list-text">Ainda não há contatos na sua agenda.
-      <a href="<?= $BASE_URL ?>create.php">Adicione um contato</a>
-    </p>
+  <p id="empty-list-text">Ainda não há contatos na sua agenda.
+    <a href="<?= $BASE_URL ?>create.php">Adicione um contato</a>
+  </p>
   <?php endif; ?>
 </div>
 <?php
