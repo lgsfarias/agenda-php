e887cf70d05bc85815f3e377aabbdbe12e5493a0
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-04-17 09:41:59 -0300

    feat: mobile layout

--- templates/header.php
+++ templates/header.php
@@ -16,9 +16,13 @@ if (isset($_SESSION['msg'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css"
+    integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Custom CSS -->
   <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css" />
   <title>Agenda de Contatos</title>
@@ -31,7 +35,7 @@ if (isset($_SESSION['msg'])) {
         <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda" />
       </a>
       <div class="navbar-nav">
-        <a href="<?= $BASE_URL ?>index.php" class="nav-link active">Home</a>
+        <a href="<?= $BASE_URL ?>index.php" id="home-link" class="nav-link active">Home</a>
         <a href="<?= $BASE_URL ?>create.php" class="nav-link active">Adicionar Contato</a>
       </div>
     </nav>
