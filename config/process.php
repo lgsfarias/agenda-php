b63a97a25dfb2568cd9bdd853cea7edfc9a3ef3d
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-04-17 09:20:34 -0300

    feat: user can add contacts to agenda

--- config/process.php
+++ config/process.php
@@ -6,16 +6,45 @@ include_once 'url.php';
 $id;
 $contact = [];
 
-if (isset($_GET['id'])) {
-  $id = $_GET['id'];
-  $sql = "SELECT * FROM contacts WHERE id = :id";
-  $stmt = $conn->prepare($sql);
-  $stmt->bindParam(':id', $id);
-  $stmt->execute();
-  $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+$data = $_POST;
+
+if (!empty($data)) {
+  // print_r($data);
+  // exit;
+
+  // Create
+  if ($data['type'] == 'create') {
+    $sql = "INSERT INTO contacts (name, phone, obs) VALUES (:name, :phone, :obs)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':name', $data['name']);
+    $stmt->bindParam(':phone', $data['phone']);
+    $stmt->bindParam(':obs', $data['obs']);
+    try {
+      $stmt->execute();
+      $_SESSION['msg'] = 'Contato criado com sucesso!';
+    } catch (PDOException $e) {
+      $_SESSION['msg'] = 'Erro ao criar contato!';
+    }
+
+    header('Location: ' . $BASE_URL . '/../index.php');
+  }
+} else {
+
+  if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM contacts WHERE id = :id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+  } else {
+
+    $sql = "SELECT * FROM contacts";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
 }
 
-$sql = "SELECT * FROM contacts";
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-$contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Close connection
+$conn = null;
