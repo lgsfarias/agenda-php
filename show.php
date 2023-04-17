6187aa6283be6ddc301c016ccc48e62cdea48472
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-04-16 21:53:06 -0300

    feat: show contact style

--- show.php
+++ show.php
@@ -2,9 +2,22 @@
 include_once 'templates/header.php';
 ?>
 <div class="container" id="view-contact-container">
+  <?php include_once 'templates/backBtn.html' ?>
   <h1 id="main-title">
     <?= $contact['name'] ?>
   </h1>
+  <p>
+    <strong>Telefone:</strong>
+  </p>
+  <p>
+    <?= $contact['phone'] ?>
+  </p>
+  <p>
+    <strong>Observação:</strong>
+  </p>
+  <p>
+    <?= $contact['obs'] ?>
+  </p>
 </div>
 <?php
 include_once 'templates/footer.php'
